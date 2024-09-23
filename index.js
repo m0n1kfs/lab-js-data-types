49f0f0c2021df60da7a8c0aa2f40964f29d72abf
Monica Fernandez Salinas <monica.fernandez.salinas@gmail.com>
2024-09-23 21:40:40 +0200

    Respuestas al lab

--- index.js
+++ index.js
@@ -8,12 +8,7 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
-
-// Print out the concatenated string
-
-
-
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5}`;
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -22,12 +17,12 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+const javA = part1.slice(0,3) + part1[3].toUpperCase();
+const scripT = part2.slice(0,5) + part2[5].toUpperCase();
+const result = javA + scripT;
 
 // Print the cameLtaiL-formatted string
-
-
-
+console.log(result)
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
@@ -35,23 +30,27 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipBT = billTotal * 0.15;
+const tipTotal = tipBT + billTotal;
 
 // Print out the tipAmount
-
-
-
+console.log(tipTotal)
 
 /*******************************************
     Iteration 2.2 | Generate Random Number
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
+let randomNumber;
+let count = 0;
 
+while (count < 1) {
+   randomNumber = Math.ceil(Math.random() * 10);
+   count++;
+}
 
 // Print the generated random number
-
-
+console.log(randomNumber);
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -62,15 +61,22 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+//false
 
 const expression2 = a || b;
+// true
 
 const expression3 = !a && b;
+// false
 
 const expression4 = !(a && b);
+// true
 
 const expression5 = !a || !b;
+// true
 
 const expression6 = !(a || b);
+// false
 
 const expression7 = a && a;
+// true
